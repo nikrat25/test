adcbed6ef212d149a52c64b14c32c92d1ce64595
nikrat25 <binomonikhil@gmail.com>
2025-08-28 15:57:51 +0530

    Create main.tf

---
+++ datafactory/main.tf
@@ -0,0 +1,17 @@
+data "azurerm_resource_group" "existing_rg" {
+  name = "rg-mars-poc"
+}
+
+resource "azurerm_data_factory" "main" {
+  name                = var.adf_name
+  location            = data.azurerm_resource_group.existing_rg.location
+  resource_group_name = data.azurerm_resource_group.existing_rg.name
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  public_network_enabled = var.public_network_enabled
+
+  tags = var.tags
+}
