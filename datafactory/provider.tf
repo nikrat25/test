8594bbea9365b91610bbaea13fdcb02b9b789775
nikrat25 <binomonikhil@gmail.com>
2025-08-28 16:00:45 +0530

    Create provider.tf

---
+++ datafactory/provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_version = ">= 1.0"
+  
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"  # Updated to version 4.x
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
+  
+  resource_provider_registrations = "none"
+}
