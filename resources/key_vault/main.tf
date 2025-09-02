faebb32f5e6edb2ab22f6de9128d2c81c478e227
nikrat25 <binomonikhil@gmail.com>
2025-09-02 18:17:47 +0530

    Update main.tf

--- resources/key_vault/main.tf
+++ resources/key_vault/main.tf
@@ -1,1 +1,18 @@
+data "azurerm_resource_group" "existing_rg" {
+  name = "rg-mars-poc"
+}
+
+resource "azurerm_key_vault" "main" {
+  name                = var.key_vault_name
+  location            = data.azurerm_resource_group.existing_rg.location
+  resource_group_name = data.azurerm_resource_group.existing_rg.name
+  tenant_id           = var.tenant_id
+  sku_name            = "standard"
+
+  soft_delete_retention_days = 7
+  purge_protection_enabled   = false
+  enable_rbac_authorization  = true
+
+  tags = var.tags
+}
 
